8728be4df0ff4dd013cd1bf9314ea6a72c8f51e3
Laura <laurabf6@gmail.com>
2026-05-20 20:10:35 +0200

    feat:TEDEN-27 add header component and main.scss and delete unnecesary style files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <header className="header">
+
+      <div className="header__logo">
+          <span className="header__logo-bullet" aria-hidden="true"></span>
+          <span className="header__logo-text">Contemporary Goddesses</span>
+      </div>
+      
+    {/* <NavBar className="header__navbar" /> */}
+
+    </header>
+  )
+}
+
+export default Header
