c3a77fbdfcfa24bbf8835bbe87a55f8393d44400
Rukayatu <rseidu941@gmail.com>
2026-05-20 14:35:43 +0200

    Save lecture Form partly completed

---
+++ src/components/SelectedFaceDownCard/SelectedFaceDownCard.jsx
@@ -0,0 +1,29 @@
+// SaveLectureForm.jsx
+// SaveLectureForm.jsx
+
+import './SaveLectureForm.scss';
+
+function SaveLectureForm() {
+  return (
+    <section className="save-lecture">
+      <h3 className="save-lecture__title">Guardar lectura</h3>
+
+      <form className="save-lecture__form">
+        <input
+          type="text"
+          placeholder="Tu nombre..."
+          className="save-lecture__input"
+        />
+
+        <button
+          type="submit"
+          className="save-lecture__button"
+        >
+          Guardar
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default SaveLectureForm;
