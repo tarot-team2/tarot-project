0b4437cd02d65fa9338be3169d40a79beea8e7e0
rseidu941-commits <rseidu941@gmail.com>
2026-05-22 13:31:53 +0200

    Feat/nav bar (#18)
    * DeleleteAllButton component partly created
    
    * component for delete all button completed
    
    * component for NavBar partly created
    
    * NavBar component completed
    
    * Remove DeleteAllButton component from App
    
    * Remove DeleteAllButton import from App component
    
    Removed import statement for DeleteAllButton component.
    
    ---------
    
    Co-authored-by: Laura <79979649+Laura-bf@users.noreply.github.com>

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,38 @@
+import './NavBar.scss';
+
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <ul className="nav-bar__list">
+        <li className="nav-bar__item">
+          <a
+            href="/"
+            className="nav-bar__link"
+          >
+            Inicio
+          </a>
+        </li>
+
+        <li className="nav-bar__item">
+          <a
+            href="/lectura"
+            className="nav-bar__link nav-bar__link--active"
+          >
+            Lectura
+          </a>
+        </li>
+
+        <li className="nav-bar__item">
+          <a
+            href="/historial"
+            className="nav-bar__link"
+          >
+            Historial
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
