d80a77011f9132ccf8998b8e7febeb3d23ca5fcf
alvi103-png <alvi103@gmail.com>
2026-05-26 18:42:51 +0200

    feat: added summary cards with SCSS, is responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import MazoPage from "./pages/mazoPage/MazoPage";
-import SummaryPage from "./pages/summaryPage/SummaryPage";
 import { Outlet } from "react-router";
 
 function App() {
+
   return (
     <>
       <Header /> 
--- src/pages/summaryPage/SummaryPage.jsx
+++ src/pages/summaryPage/SummaryPage.jsx
@@ -9,7 +9,16 @@ const SummaryPage = () => {
   const navigate = useNavigate()
   const [userName, setUserName] = useState('')
 
-  useEffect(() => {
+  const fakeCard = {
+
+    arcaneNumber: "Arcano 0",
+    name: "El Loco",
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/90/RWS_Tarot_00_Fool.jpg',
+    meaning: "El viaje del Loco hacia el Mundo comienza en el número 0, un punto incierto donde todo es posible y se requiere una gran dosis de fe. Esta carta representa a quienes se aventuran en territorios inexplorados y llenos de misterio. Es la manifestación del impulso de lanzarse al abismo sin garantías.",
+    goddessName: "Fei-Fei Li"
+  }
+
+  /*useEffect(() => {
     if (!state?.selectedCards) {
       navigate('/mazo')
     }
@@ -17,7 +26,7 @@ const SummaryPage = () => {
 
   if (!state?.selectedCards) {
     return null
-  }
+  }*/
 
   const { past, present, future } = state.selectedCards
 
@@ -36,9 +45,9 @@ const SummaryPage = () => {
       </section>
 
       <section className="summary-page__cards">
-        <SummaryCard position="past"    card={past} />
-        <SummaryCard position="present" card={present} />
-        <SummaryCard position="future"  card={future} />
+        <SummaryCard position="past"    card={fakeCard} />
+        <SummaryCard position="present" card={fakeCard} />
+        <SummaryCard position="future"  card={fakeCard} />
       </section>
 
       <section className="summary-page__save">
