74a7447009c180da2c3ec42585c1c511bb1fef1b
rseidu941-commits <rseidu941@gmail.com>
2026-05-26 19:38:57 +0200

    Tden 47 historypage (#25)
    * Save lecture Form partly completed
    
    * style being added to the savelectureform
    
    * added features to make componnets flexible and reusable
    
    * content added to the history page
    
    ---------
    
    Co-authored-by: Laura <79979649+Laura-bf@users.noreply.github.com>
    Co-authored-by: Damaris Castro <damaris.icastro@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import MazoPage from "./pages/mazoPage/MazoPage";
-import SummaryPage from "./pages/summaryPage/SummaryPage";
 import { Outlet } from "react-router";
-import MazoPage from "./pages/MazoPage/MazoPage";
 
 function App() {
   return (
     <>
-      <Header /> 
+      <Header />
       <main>
         <Outlet />
       </main>
