6eaecbda0848a926d3dc8f06b960f778942633dc
alvi103-png <alvi103@gmail.com>
2026-05-26 14:13:01 +0200

    fix: changed some imports in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Footer from "./components/Footer/Footer";
 import MazoPage from "./pages/mazoPage/MazoPage";
 import SummaryPage from "./pages/summaryPage/SummaryPage";
 import { Outlet } from "react-router";
-import MazoPage from "./pages/MazoPage/MazoPage";
 
 function App() {
   return (
