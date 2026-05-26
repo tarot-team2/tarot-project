34a30493079e1c8f93d3bfe9002c47d5e0ab0951
Rukayatu <rseidu941@gmail.com>
2026-05-26 13:24:20 +0200

    content added to the history page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 function App() {
   return (
     <>
+    <header/>
+    <main/>
+    <historypage/>
+    <footeer/>
     </>
   )
 }
---
+++ src/pages/historyPage/historyPage.jsx
@@ -0,0 +1,20 @@
+import Header from "../components/Header/Header";
+function historypage () {
+return (
+    <>
+    
+
+    <main classname="history-page">
+        <h1>Historial</h1>
+        <section classname="history-page_list">
+           <header/>
+           
+        
+        </section>
+
+        </main>
+    </>
+)
+
+}
+export default historypage;
