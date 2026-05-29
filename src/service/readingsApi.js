efda24b835178c713b9437dc4fe409cde7992287
Damaris Castro <damaris.icastro@gmail.com>
2026-05-29 09:57:42 +0200

    D del CRUD

--- src/service/readingsApi.js
+++ src/service/readingsApi.js
@@ -4,32 +4,21 @@ const API_URL = 'http://localhost:3000/readings'
 //Obtener la lectura
 export const getReadings = async () => {
     const response = await axios.get(API_URL)
-    return response.data
+    return response.data.reverse()
 }
 //Guardar la lectura
 export const saveReading = async (reading) => {
-    const response = await fetch(API_URL, {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(reading),
-    })
-    return response.json()
-    
+    const response = await axios.post(API_URL, reading)
+    return response.data
 }
+
 //Eliminar la lectura
 export const deleteReading = async (id) => {
-    await fetch(`${API_URL}/${id}`, {
-    method: 'DELETE',
-})
+    await axios.delete(`${API_URL}/${id}`)
 }
 
 //Actualizar la lectura
 export const updateReading = async (id, updateData) => {
-    const response = await axios.patch(
-        `${API_URL}/${id}`,
-        updateData
-    )
+    const response = await axios.patch(`${API_URL}/${id}`, updateData)
     return response.data
 }
